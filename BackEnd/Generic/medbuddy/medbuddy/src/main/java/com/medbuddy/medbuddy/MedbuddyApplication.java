a9942ff6b4ff0e8bbd030476716e42a6491b4b16
Vlad Mocanu <vladmocanu2003@gmail.com>
2024-04-24 20:17:50 +0300

    added the spring project

---
+++ BackEnd/Generic/medbuddy/medbuddy/src/main/java/com/medbuddy/medbuddy/MedbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.medbuddy.medbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedbuddyApplication.class, args);
+	}
+
+}
---
+++ BackEnd/Generic/medbuddy/medbuddy/src/test/java/com/medbuddy/medbuddy/MedbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medbuddy.medbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
