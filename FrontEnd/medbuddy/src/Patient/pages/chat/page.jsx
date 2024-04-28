3862d03f34ece35d18f915eb865b55406c8bc915
beth23eli <secrieru.eliza@gmail.com>
2024-04-28 11:01:45 +0300

    MedBuddy Chat: message-area added, and functionality added when sending a message(iteratia2)

--- FrontEnd/medbuddy/src/Patient/pages/chat/page.jsx
+++ FrontEnd/medbuddy/src/Patient/pages/chat/page.jsx
@@ -1,14 +1,36 @@
-import React from "react";
 import styles from "./page.module.scss";
 import Send from "../../images/send-msg.svg";
 import Choose from "../../images/add-files.svg";
-import { useState } from "react";
-export default function ChatPage(){
+import React, { useState, useEffect } from "react";
+
+export default function ChatPage() {
     const [message, setMessage] = useState('');
-    const [file, setFile] = useState(null);
+    const [messages, setMessages] = useState([]);
+    const [isNewMessage, setIsNewMessage] = useState(false); 
+
+    const handleMessageChange = (e) => {
+        setMessage(e.target.value);
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault(); // Evită comportamentul implicit al apăsării tastei Enter (de a face un newline)
+            handleSend(); // Trimite mesajul
+        }
+    };
 
     const handleSend = () => {
-        // handle sending the message and file here
+        if (message.trim() !== '') {
+            const newMessage = { id: Date.now(), text: message };
+            setMessages([...messages, newMessage]); // Adaugă noul mesaj în array-ul de mesaje
+            setMessage(''); // Resetează caseta de text după trimitere
+
+            setIsNewMessage(true);
+            // Resetarea state-ului isNewMessage după 2 secunde
+            setTimeout(() => {
+                setIsNewMessage(false);
+            }, 2000);
+        }
     };
 
     const handleNewConvo = () => {
@@ -17,21 +39,24 @@ export default function ChatPage(){
     return(
         <>
             <div className={`${styles.page__message_area}`}>
-                <div className={`${styles.page__message_container}`}>
-
+            <div className={`${styles.page__message_container} `}>
+                    {messages.map((msg) => (
+                            <div key={msg.id}  className={styles.page__message}>{msg.text}</div>
+                    ))}
                 </div>
                 <div className={`${styles.page__message_buttons}`}>
                     <button className={`${styles.page__message_buttons_action}`} to="/">
-                        Back to Welcome
+                        Back to Homepage
                     </button>
                     <button className={`${styles.page__message_buttons_action}`} onClick={handleNewConvo}>
-                        New Discution
+                        New Discussion
                     </button>
                 </div>
                 <div className={`${styles.page__message_input}`}>
                     <textarea 
                         className={`${styles.page__message_area_question}`} 
-                        placeholder="Ask your question" />
+                        placeholder="Ask your question" value={message}
+                        onChange={handleMessageChange} onKeyPress={handleKeyPress} />
                     <div className={`${styles.page__message_area_actions}`}>
                         <label className={`${styles.page__message_area_file}`}>
                             <img 
