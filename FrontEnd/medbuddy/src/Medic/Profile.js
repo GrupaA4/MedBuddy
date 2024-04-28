3cf4e4938f900205d57974eb224716a79bf08186
laur040 <lauramironov16@gmail.com>
2024-04-28 20:17:44 +0300

    update buttons

--- FrontEnd/medbuddy/src/Medic/Profile.js
+++ FrontEnd/medbuddy/src/Medic/Profile.js
@@ -2,7 +2,7 @@
 import './Profile.css';
 import Footer from './Footer' ;
 import Navbar from './Navbar' ;
-
+import Logo from './logo.png';
 
 
 function Profile() {
@@ -15,7 +15,17 @@ function Profile() {
       </div>
 
         <div className="content">
-
+          <div className="left-container">
+          <div className="profile-picture">p
+          </div>
+        <div className="buttons-container">
+        <a href="#" className="button">Edit Profile</a>
+        <a href="#" className="button">Change Password</a>
+        <a href="#" className="button">Delete Account</a>
+        </div>
+      
+          </div>
+          <div className="general-information"></div>
         </div>
 
         <div className="footer-container"><Footer/>
