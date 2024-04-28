01700b55ba81fdb8049c0ae10549b9577517c891
Anda-Moraru <anda.moraru26@gmail.com>
2024-04-28 17:28:00 +0300

    update profile

---
+++ FrontEnd/medbuddy/src/Medic/Profile.js
@@ -0,0 +1,38 @@
+import logo from './logo.png';
+import './Profile.css';
+import Footer from './Footer' ;
+
+
+
+function Profile() {
+  return (
+
+    <div className="Profile">
+      
+      <div className="navbar">
+      <img src={logo} className="logo" alt="logo" />
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Settings</a></li>
+            </ul>
+
+        </div>
+
+        <div className="content">
+
+        </div>
+
+        <div className="footer-container"><Footer/>
+        </div>
+
+        
+
+
+
+    </div>
+  );
+}
+
+export default Profile;
