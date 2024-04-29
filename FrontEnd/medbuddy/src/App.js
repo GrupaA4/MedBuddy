54c97fd6edc176b15c251fafd27a25319553b4a8
laur040 <lauramironov16@gmail.com>
2024-04-29 17:12:21 +0300

    modified app.js

--- FrontEnd/medbuddy/src/App.js
+++ FrontEnd/medbuddy/src/App.js
@@ -7,15 +7,12 @@ import Register from './Medic/pages/Register';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
 
       <Register />
 
-=======
-        <Profile/>
+    
 
     
->>>>>>> 5e570a9a15f4935661ee30cb42b75f0cfd6174c2
     </div>
   );
 }
