968f5e4d5fc35742f63eba465951647d75d7f1ec
Anda-Moraru <anda.moraru26@gmail.com>
2024-05-17 23:15:58 +0300

    updateut

--- FrontEnd/medbuddy/src/App.js
+++ FrontEnd/medbuddy/src/App.js
@@ -5,6 +5,7 @@ import Conversation from './Medic/navigation/Conversation/pages/Conversation';
 import Profile from './Medic/navigation/Profile/Profile';
 import Register from './Medic/navigation/Register/Register';
 import MainPage from './Medic/navigation/MainPage/Conversations';
+import Contact from './Medic/navigation/Contact/Contact';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
     <div className="App">
 
 
-      <MainPage />
+      <Contact />
 
     </div>
   );
---
+++ FrontEnd/medbuddy/src/Medic/navigation/Contact/Contact.js
@@ -0,0 +1,46 @@
+import './Contact.css';
+import Footer from '../common-components/Footer' ;
+import Navbar from '../common-components/Navbar' ;
+import Logo from '../common-components/logoB.png';
+import Forum from './Forum';
+
+function Contact() {
+    return (
+  
+      <div className="Contact">
+        <div className="body">
+        <div className='navbar-container'>
+          <Navbar/>
+        </div>
+  
+          <div className="content">
+            <div className="left-container">
+            <h3>Contact&Support</h3>
+                <p class="paragraph">Having some problems?
+                The MedBuddy team is eager to listen to you and provide a better experience to everyone.
+                Contact us: </p>
+                <ul class="para">
+                    <li>Email: support@MedBuddy.com</li>
+                    <li>Phone: 123-456-7890</li>
+                </ul>
+      
+            </div>
+            
+            <div className="forum-container">
+                <Forum/>
+            
+            </div>
+          </div>
+          </div>
+          <div className="footer-container"><Footer/></div>
+          
+          
+  
+  
+  
+      </div>
+    );
+  }
+  
+  export default Contact;
+  
