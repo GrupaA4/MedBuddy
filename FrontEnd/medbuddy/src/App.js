f0c1f17cb27011b065b1adda68ec87ed20b534fb
BlinduAndrada <andradablindu15@gmail.com>
2024-05-26 22:12:04 +0300

    migrate 2.0

--- FrontEnd/medbuddy/src/App.js
+++ FrontEnd/medbuddy/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './Admin/main_page/main_page';
 import LoginPage from './Admin/log-in_page/log-in_page';
+import AdminMainPage from './Admin/admin_main_page/admin_main_page';
+import ReportPage from './Admin/report_page/report_page';
+import UserPage from './Admin/user_page/user_page';
 
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={MainPage} />
-        <Route path="/login" component={LoginPage} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<AdminMainPage />} />
+        <Route path="/report" element={<ReportPage />} />
+        <Route path="/user" element={<UserPage />} />
+      </Routes>
     </Router>
   );
 };
