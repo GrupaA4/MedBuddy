a6cd7514dcaf3bc2b9eb5415169490c958dd6f37
BlinduAndrada <andradablindu15@gmail.com>
2024-05-27 13:07:06 +0300

    Daiana-branch and Stefan-branch merged

--- FrontEnd/medbuddy/src/App.js
+++ FrontEnd/medbuddy/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './Admin/main_page/main_page';
 import LoginPage from './Admin/log-in_page/log-in_page';
 import AdminMainPage from './Admin/admin_main_page/admin_main_page';
-import ReportPage from './Admin/report_page/report_page';
+import ReportPage from './Admin/admin_report_page/admin_report_page';
 import UserPage from './Admin/admin_user_page/admin_user_page';
 
 
