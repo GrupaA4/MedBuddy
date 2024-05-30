2fe642d5870067d4a9b2a272c4fe67cfa958fb96
unknown <lau.m.daiana@gmail.com>
2024-05-30 14:56:24 +0300

    admin user-page fetch done

--- FrontEnd/medbuddy/src/Admin/admin_user_page/admin_user_page.js
+++ FrontEnd/medbuddy/src/Admin/admin_user_page/admin_user_page.js
@@ -7,7 +7,6 @@ import Logo from '../assets/MedBuddy.png';
 
 const UserPage = () => {
   const[medics,setMedics]=useState([]);
-  const [selectedMedic, setSelectedMedic] = useState([]);
   const [medicId,setMedicId] = useState([]);
   const [medicFirstName,setMedicFirstName]= useState([]);
   const [medicLastName,setMedicLastName]= useState([]);
@@ -20,23 +19,53 @@ const UserPage = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
+
+  const getCookieValue = (name) => {
+    const cookies = document.cookie.split(";").map((cookie) => cookie.trim());
+    for (const cookie of cookies) {
+      if (cookie.startsWith(name + "=")) {
+        return cookie.substring(name.length + 1);
+      }
+    }
+    return null;
+  };
+
+  const emailFromCookie = getCookieValue("user_email");
+  const passwordFromCookie = getCookieValue("user_pass");
+  const authorisation = btoa(`${emailFromCookie}:${passwordFromCookie}`);
+
+
  
   
   useEffect(() => {
     const fetchMedics = async () => {
       try {
         const response = await fetch('http://localhost:7264/medbuddy/seerequestingmedics',{
-          method:'GET'
+          method:'GET',
+          headers: {
+            Authorization: `Basic ${authorisation}`,
+          },
         });
-        if (response.ok) {
+        if (response.status == 200) {
           const data = await response.json();
+          setMedicId(data.medics[0].medicId);
+
           const medicDetailsPromises = data.medics.map(async (medicId) => {
             const medicResponse = await fetch(`http://localhost:7264/medbuddy/viewmedicprofile/${medicId}`,
-              {method:'GET'}
+              {method:'GET',
+              headers: {
+                Authorization: `Basic ${authorisation}`,
+              },
+              }
             );
             
             if (medicResponse.ok) {
               const medic = await medicResponse.json();
+
+              setMedicEmail(medic.email);
+              setMedicFirstName(medic.firstName);
+              setMedicLastName(medic.lastName);
+              setMedicLicense(medic.medicLicense);
             
           }
             return null;
@@ -59,6 +88,9 @@ const UserPage = () => {
     try {
       const response = await fetch(`http://localhost:7264/medbuddy/allowmedic/${medicId}`, {
         method: 'PATCH',
+        headers: {
+          Authorization: `Basic ${authorisation}`,
+        },
       });
 
       if (response.ok) {
@@ -75,6 +107,9 @@ const UserPage = () => {
     try{
       const response=await fetch('http://localhost:7264/medbuddy/softdeleteuser/:${medicId}', {
         method:'PATCH',
+        headers: {
+          Authorization: `Basic ${authorisation}`,
+        },
       });
   
       if(response.ok){
@@ -89,29 +124,11 @@ const UserPage = () => {
   };
 
   //ASTA NU MERGE INCA
-  const handleLicenseCheck = async (medicId) => {
+  /*const handleLicenseCheck = async (medicId) => {
     try {
-      const response = await fetch(`http://localhost:7264/medbuddy/viewmedicprofile/${medicId}`,{
-        method:'GET'
-      });
-      if (response.ok) {
-        const medicProfile = await response.json();
-        setSelectedMedic(medicProfile);
-      } else {
-        console.error('Failed to fetch medic profile');
-      }
-    } catch (error) {
-      console.error('Error fetching medic profile:', error);
+    
     }
-  };
-
-
-  const indexOfLastMedic = currentPage * medicsPerPage;
-  const indexOfFirstMedic = indexOfLastMedic - medicsPerPage;
-  const currentMedics = medics.slice(indexOfFirstMedic, indexOfLastMedic);
-
-  
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  };*/
 
 
   return (
@@ -145,29 +162,29 @@ const UserPage = () => {
         <div className={styles.admin_user_page_container1__header}>
           <p className={styles.admin_user_page_container1__header__text}>Unverified accounts</p>
           <div className={styles.admin_user_page_container1__buttons}>
-          <button className={styles.admin_user_page_container1__before } onClick={() => paginate(currentPage - 1)}
-          disabled={currentPage === 1}>&#8678;</button>
-            <button className={styles.admin_user_page_container1__next}onClick={() => paginate(currentPage + 1)}
-          disabled={currentMedics.length < medicsPerPage}>&#8680;</button>
+          <button className={styles.admin_user_page_container1__before } /*onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}*/>&#8678;</button>
+            <button className={styles.admin_user_page_container1__next} /*onClick={() => paginate(currentPage + 1)}
+          disabled={currentMedics.length < medicsPerPage}*/>&#8680;</button>
           </div>
         </div>
 
-        {[...Array(5).keys()].map(i => (
+        {medics.map((medic,i)=> (
           <div className={styles.admin_user_page_container1__square}>
             <div className={styles.admin_user_page_container1__square__icon_and_data}>
             <div className={styles.admin_user_page_container1__square__icon}>
               <p>PHOTO</p>
             </div>
             <div className={styles.admin_user_page_container1__square__data}>
-            <div className={styles.admin_user_page_container1__square__data__info}>NAME: <span className={styles.name}>{medic.firstName} {medic.lastName}</span></div>
-                <div className={styles.admin_user_page_container1__square__data__info}> EMAIL: <span className={styles.email}>{medic.email}</span></div>
+            <div className={styles.admin_user_page_container1__square__data__info}>NAME: <span className={styles.name}>*{medic.medicLastName} {medic.medicFirstName}</span></div>
+                <div className={styles.admin_user_page_container1__square__data__info}> EMAIL: <span className={styles.email}>{medic.medicEmail}</span></div>
                 <div className={styles.admin_user_page_container1__square__data__info}>MEDICAL LICENSE: <span className={styles.medicalLicense}>{medic.medicalLicense}</span></div>
             </div>
             </div>
              <div className={styles.admin_user_page_container1__square__data__buttons}>
-              <button className={styles.admin_user_page_container1__button1} type="button" onClick={() => handleAccept(medic.id)}>Accept</button>
-              <button className={styles.admin_user_page_container1__button2} type="button" onClick={() => handleDeny(medic.id)}>Deny</button>
-              <button className={styles.admin_user_page_container1__button3} type="button" onClick={() => handleLicenseCheck(medic.id)}>Check License</button>
+              <button className={styles.admin_user_page_container1__button1} type="button" onClick={() => handleAccept(medic.medicId)}>Accept</button>
+              <button className={styles.admin_user_page_container1__button2} type="button" onClick={() => handleDeny(medic.medicId)}>Deny</button>
+              <button className={styles.admin_user_page_container1__button3} type="button" /* onClick={() => handleLicenseCheck(medic.medicId)}*/>Check License</button>
             </div>
           </div>
         ))}
@@ -179,7 +196,6 @@ const UserPage = () => {
             <p>PHOTO</p>
           </div>
           <div className={styles.admin_user_page_container2__text1}>Hey Admin!</div>
-          <div className={styles.admin_user_page_container2__text2}>Do one of the following actions below:</div>
           
         </div>
         <button className={styles.admin_user_page_container2__button1} type="button">Accept</button>
