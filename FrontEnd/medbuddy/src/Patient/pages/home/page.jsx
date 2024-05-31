ead154dbf19569fe9a5a5eabc1b39035cfc86805
beth23eli <secrieru.eliza@gmail.com>
2024-05-31 10:52:25 +0300

    homepage updated(it4)

--- FrontEnd/medbuddy/src/Patient/pages/home/page.jsx
+++ FrontEnd/medbuddy/src/Patient/pages/home/page.jsx
@@ -49,6 +49,9 @@ export default function Home(){
         setEmail('');
         
     };
+    const handleBtn = () => {
+        window.location.href = '/chat';
+    };
 
     const [isMobileMenu, setIsMobileMenu] = useState(false);
 
@@ -67,6 +70,19 @@ export default function Home(){
                 </div>
                 <img className={`${styles.welcome_section__image}`} src={BrainVector} alt='Cute Brain that can be yours'/>
             </section>
+
+            <section className={`${styles.get_started_section}`}>
+                <div className={`${styles.get_started_section__text}`}>
+                    <h1 className={`${styles.get_started_section__title}`}>
+                        What are you waiting for?
+                    </h1>
+                    <p className={`${styles.get_started_section__subtitle}`}>Start a conversation  with our Virtual Assistant!</p>
+                </div>
+                <button className={`${styles.get_started_section__btn}`} onClick={handleBtn}>
+                    Click Here To Start <FontAwesomeIcon icon={faArrowRight} className={styles.get_started_section__btn_icon}/>
+                </button>
+            </section>
+            
             <section className={`${styles.motivation_section}`}>
                 <div className={`${styles.motivation_section__text}`}>
                     <h1 className={`${styles.motivation_section__title}`}>
@@ -77,63 +93,6 @@ export default function Home(){
                     </p>
                 </div>
                 <img className={`${styles.motivation_section__image}`} src={TechImg} alt='Beautiful'/>
-            </section>
-            <section className={`${styles.get_started_section}`}>
-                <div className={`${styles.get_started_section__text}`}>
-                    <h1 className={`${styles.get_started_section__title}`}>
-                        What are you waiting for?
-                    </h1>
-                    <p className={`${styles.get_started_section__subtitle}`}>Start a conversation  with our Virtual Assistant!</p>
-                </div>
-                <button type="submit" className={`${styles.get_started_section__btn}`}>
-                    Click Here To Start <FontAwesomeIcon icon={faArrowRight} className={styles.get_started_section__btn_icon}/>
-                </button>
-            </section>
-            <section className={`${styles.form_section}`}>
-                <h3 className={`${styles.form_section__title}`}>We are continuously improving! Give us a Feedback!</h3>
-                <form onSubmit={handleSubmit} className={`${styles.form_section__form}`}>
-                    <div className={`${styles.form__first_row}`}>
-                        <div className={`${styles.form__first_row__item}`}>
-                            <label htmlFor="firstName">First Name:</label>
-                            <input
-                                type="text"
-                                id="firstName"
-                                value={firstName}
-                                onChange={handleFirstNameChange} required
-                                placeholder="Write your name here..."
-                            />
-                        </div>
-                        <div className={`${styles.form__first_row__item}`}>
-                            <label htmlFor="lastName">Last Name:</label>
-                            <input
-                                type="text"
-                                id="lastName"
-                                value={lastName}
-                                onChange={handleLastNameChange} required placeholder="Write your surname here..."/>
-                                
-                        </div>
-                    </div>
-                    <div className={`${styles.form__email}`}>
-                        <label htmlFor="email">Email:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            value={email}
-                            onChange={handleEmailChange} required
-                            placeholder="Write your email here..."
-                        />
-                    </div>
-                    <div className={`${styles.form__msg}`}>
-                        <label htmlFor="feedback">Message:</label>
-                        <textarea
-                            className={`${styles.form_section__feedback_input}`}
-                            value={feedback}
-                            onChange={handleFeedbackChange} required
-                            placeholder="Write your feedback here..."
-                        ></textarea>
-                    </div>
-                    <button type="submit" className={`${styles.form__btn}`}>Submit</button>
-                </form>
             </section>
             </>
         }
