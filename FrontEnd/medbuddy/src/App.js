46eeeffb170706535c1488885d89f3304c063bb6
laur040 <lauramironov16@gmail.com>
2024-06-02 12:16:17 +0300

    route update

--- FrontEnd/medbuddy/src/App.js
+++ FrontEnd/medbuddy/src/App.js
@@ -5,12 +5,22 @@ import LoginPage from "./Admin/log-in_page/log-in_page";
 import AdminMainPage from "./Admin/admin_main_page/admin_main_page";
 import ReportPage from "./Admin/admin_report_page/admin_report_page";
 import UserPage from "./Admin/admin_user_page/admin_user_page";
-import Register from "./Medic/navigation/Register/Register";
+
+import RegisterMedic from "./Medic/navigation/Register/Register";
 import RegisterPatient from "./Patient/pages/register/register";
 import HomeMedic from "./Medic/navigation/MainPage/Conversations";
-import Contact from "./Medic/navigation/Contact/Contact";
-import SettingsMedic from "./Medic/navigation/Profile/Profile";
+import ContactMedic from "./Medic/navigation/Contact/Contact";
+import ProfileMedic from "./Medic/navigation/Profile/Profile";
 import Diagnostic from "./Medic/navigation/Diagnoses/Diagnoses";
+
+import Home from './Patient/pages/home/page';
+import ChatPage from './Patient/pages/chat/page';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Diagnoses from './Patient/pages/diagnoses/page';
+// import Register from './Patient/pages/register/register';
+import ProfilePatient from './Patient/pages/profile/profile';
+import Contact from './Patient/pages/contact/page';
+import About from './Patient/pages/about/page';
 
 const App = () => {
   return (
@@ -21,12 +31,15 @@ const App = () => {
         <Route path="/admin" element={<AdminMainPage />} />
         <Route path="/report" element={<ReportPage />} />
         <Route path="/user" element={<UserPage />} />
-        <Route path="/register" element={<Register />} />
+
+        <Route path="/registerMedic" element={<RegisterMedic />} />
         <Route path="/registerPatient" element={<RegisterPatient />} />
         <Route path="/homeMedic" element={<HomeMedic />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/settingsMedic" element={<SettingsMedic />} />
+        <Route path="/contactMedic" element={<ContactMedic />} />
+        <Route path="/profileMedic" element={<ProfileMedic />} />
         <Route path="/diagnoses" element={<Diagnostic />} />
+
+        
       </Routes>
     </Router>
   );
