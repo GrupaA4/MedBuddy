ecddde6e1dfd622db9d574bf4859cc6249ff3204
mat <128747016+mocanumatei31@users.noreply.github.com>
2024-06-07 02:03:17 +0300

    Changed MedbuddyUtil

--- BackEnd/Generic/medbuddy/medbuddy/src/main/java/com/medbuddy/medbuddy/services/MessagerieService.java
+++ BackEnd/Generic/medbuddy/medbuddy/src/main/java/com/medbuddy/medbuddy/services/MessagerieService.java
@@ -55,9 +55,9 @@ public class MessagerieService {
         return medbuddyUtil.receiveMessageFromMedbuddy(userId);
     }
 
-    public void sendMessageToMedbuddy() {
+    public void sendMessageToMedbuddy(String message) {
         UUID userId = userDAO.getUserId(SecurityUtil.getEmail());
-        medbuddyUtil.sendMessageToMedbuddy(userId);
+        medbuddyUtil.sendMessageToMedbuddy(message, userId);
     }
 
     /*@Autowired
--- BackEnd/Generic/medbuddy/medbuddy/src/main/java/com/medbuddy/medbuddy/utilitaries/MedbuddyUtil.java
+++ BackEnd/Generic/medbuddy/medbuddy/src/main/java/com/medbuddy/medbuddy/utilitaries/MedbuddyUtil.java
@@ -1,9 +1,16 @@
 package com.medbuddy.medbuddy.utilitaries;
 
 import com.medbuddy.medbuddy.models.Message;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+
+import static java.nio.file.StandardWatchEventKinds.*;
 
 @Component
 public class MedbuddyUtil {
@@ -12,14 +19,67 @@ public class MedbuddyUtil {
         //to be added
     }
 
-    public Message receiveMessageFromMedbuddy(UUID userId) {
-        return new Message("Test message");
+    public static Message receiveMessageFromMedbuddy(UUID userId) {
+        try {
+                WatchService watcher = FileSystems.getDefault().newWatchService();
+                Path directory = Paths.get("Backend");
+                directory.register(watcher, ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE);
+                while (true) {
+                    WatchKey key = watcher.take();
+                    List<WatchEvent<?>> events = key.pollEvents();
+                    boolean done = false;
+                    for (WatchEvent<?> event : events) {
+                        if (event.kind() == ENTRY_MODIFY) {
+                            done = true;
+                            Path modifiedFile = (Path) event.context();
+                            System.out.println("File modified: " + modifiedFile);
+                            FileReader reader = new FileReader("Backend" + File.separator + modifiedFile.toFile());
+                            BufferedReader bufferedReader = new BufferedReader(reader);
+                            String line;
+                            List<String> inputs = new ArrayList<>();
+                            while ((line = bufferedReader.readLine()) != null) {
+                                inputs.add(line);
+                            }
+                            String message = null;
+                            String category = null;
+                            String diagnostic = null;
+                            if(!inputs.isEmpty()) {
+                                message = inputs.get(0);
+                                if (inputs.size() > 1) {
+                                    category = inputs.get(1);
+                                    if (message.equalsIgnoreCase("Done") && inputs.size() > 2) {
+                                        diagnostic = inputs.get(2);
+                                    }
+                                }
+                            }
+                            bufferedReader.close();
+                            return new Message(message);
+                        }
+                    }
+                    key.reset();
+                }
+            }
+            catch (IOException | InterruptedException e){
+            e.printStackTrace();
+            return null;
+        }
+        //medbuddyComUtoG
         //receive message
         //check for diagnosis
         //  if diagnosis, then create a MedicalHistoryEntry
     }
 
-    public void sendMessageToMedbuddy(UUID userId) {
+    public static void sendMessageToMedbuddy(String messageToBeSent, UUID userId) {
         //send message
+        try {
+            File inputFile = new File("Backend" + File.separator + "medbuddyComGtoU.txt");
+            FileWriter writer = new FileWriter(inputFile);
+            writer.write(messageToBeSent);
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
