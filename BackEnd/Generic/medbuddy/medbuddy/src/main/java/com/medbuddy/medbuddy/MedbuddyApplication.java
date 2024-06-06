e63b3e1570c14bae6c0d0a74fe1860b29efa382e
POONeculauEduardStefan <125999819+POONeculauEduardStefan@users.noreply.github.com>
2024-06-06 19:54:57 +0300

    Ceva interesant

--- BackEnd/Generic/medbuddy/medbuddy/src/main/java/com/medbuddy/medbuddy/MedbuddyApplication.java
+++ BackEnd/Generic/medbuddy/medbuddy/src/main/java/com/medbuddy/medbuddy/MedbuddyApplication.java
@@ -15,15 +15,10 @@ import java.util.UUID;
 public class MedbuddyApplication {
 
 	public static void main(String[] args) throws SQLException {
-<<<<<<< HEAD
 
 		String currentDir = System.getProperty("user.dir");
 		System.out.println("Current working directory: " + currentDir);
 
-=======
-		String currentDir = System.getProperty("user.dir");
-		System.out.println("Current working directory: " + currentDir);
->>>>>>> 0cfd717c7177d42b235c9029e69faf03ec4d8c13
 		SpringApplication.run(MedbuddyApplication.class, args);
 	}
 
